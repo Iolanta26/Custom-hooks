67f5599415b855f7c1857802cb799f2998db8b05
Iolanta26 <iolanta.u@gmail.com>
2021-11-04 11:16:35 +0300

    custom hook app

---
+++ src/components/Tag_V1.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+const Random = () => {
+  const [tag, setTag] = useState("");
+  const [gif, setGif] = useState("");
+
+  const fetchGif = async (tag) => {
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+
+    const { data } = await axios.get(url);
+
+    const imageSrc = data.data.images.downsized_large.url;
+
+    setGif(imageSrc);
+  };
+
+  useEffect(() => {
+    fetchGif("dogs");
+  }, []);
+
+  const handleClick = () => {
+    fetchGif(tag);
+  };
+
+  return (
+    <div className="container">
+      <h1>Random {tag} Gif</h1>
+      <img width="500" src={gif} alt="Random Gif" />
+      <input
+        placeholder="type here"
+        value={tag}
+        onChange={(e) => setTag(e.target.value)}
+      />
+      <button onClick={handleClick}>CLICK FOR NEW</button>
+    </div>
+  );
+};
+
+export default Random;
